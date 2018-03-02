963a904c7f992766c8129d2898faad39103d205e
Bradley Sloan <bradsloan16@gmail.com>
2018-03-02 15:16:43 -0500

    add lawfirm

--- index.php
+++ index.php
@@ -87,10 +87,7 @@
 							<article>
 								<a href="https://movie-match-sloan.herokuapp.com/" target="_blank" class="image featured"><img src="images/hkwg.jpg" alt="" /></a>
 								<header>
-									<h3><a href=https://movie-match-sloan.herokuapp.com/" target="_blank">MovieMatch</a></h3>
-								</header>
-								<header>
-									<h3><a href=https://hkwglaw.com" target="_blank">Hurwitz & Gantz Law Firm</a></h3>
+									<h3><a href="https://movie-match-sloan.herokuapp.com/" target="_blank">Hurwitz & Gantz Law Firm</a></h3>
 								</header>
 								<p>Hurwitz & Gantz, a Michigan based law firm, hired me to remake their website. For this project, I utilized a React front-end for a quick and clean interface. I also added an email feature so that each lawyer may be contacted directly from the website.
 								</p>
